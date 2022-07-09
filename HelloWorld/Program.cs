9edd9aaf90932ece424d76cf81b8ce4bfda414eb
RobelTebeje <robel.tebeje.ao5500@gmail.com>
2022-07-09 16:17:31 +0300

    Initial commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿//C# shortens our code by using top level statements
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
